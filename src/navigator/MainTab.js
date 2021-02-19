4da8c7fd844f677a92278c18651ad74d7c5bf2b3
Luis Gustavo <gustascrows@gmail.com>
2021-02-19 20:33:49 -0300

    Feat: Main tab remodel

--- src/navigator/MainTab.js
+++ src/navigator/MainTab.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import TabHomeScreen from '../views/HomeScreen';
-import TabAboutScreen from '../views/AboutScreen';
+import TabHomeScreen from '../views/TabHomeScreen';
+import TabAboutScreen from '../views/TabAboutScreen';
+import TabBuySell from '../views/TabBuySellScreen';
 const Tab = createBottomTabNavigator();
 import { Image } from 'react-native';
 
@@ -14,19 +15,25 @@ export default function MainTab() {
 
     switch(route.name){
     case 'TabHome':
-    imgSrc = require('../img/a.png');
+    imgSrc = require('../img/nano.png');
     break;
     case 'About' :
-        imgSrc = require('../img/ah.png');
+        imgSrc = require('../img/blockchain.png');
         break;
+        case 'BuySell' :
+            imgSrc = require('../img/money.png');
+            break;
+        
     }
-
+   
      return <Image source={imgSrc} style={{width:24 , height:24}} />;
     }
     })}
     >
-        <Tab.Screen name='TabHome' component={TabHomeScreen} options={{tabBarLabel:'Home'}} />
+        <Tab.Screen name='Home' component={TabHomeScreen} options={{tabBarLabel:'Home'}} />
         <Tab.Screen name='About' component={TabAboutScreen} options={{tabBarLabel:'Sobre'}} />
+        <Tab.Screen name='BuySell' component={TabBuySell} options={{tabBarLabel:'Buy&Sell'}} />
+
     </Tab.Navigator>
     );
 }
