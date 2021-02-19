4965a840b36a9c36584328a290d8816468bdbfde
Luis Gustavo <gustascrows@gmail.com>
2021-02-19 20:34:00 -0300

    Feat: Views iniciais

---
+++ src/views/TabBuySellScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function BuySell() {
+    return (
+        <View>
+            <Text>Home Tab</Text>
+        </View>
+    );
+}
+
+export default BuySell;  
